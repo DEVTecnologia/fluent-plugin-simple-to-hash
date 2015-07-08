a7d32a79a41f76ec17d1628069524b89a09f9c80
Augusto Nishi <augusto.nishi@gmail.com>
2015-07-08 19:23:09 -0300

    Fix names

--- lib/fluent/plugin/filter_simple2hash.rb
+++ lib/fluent/plugin/filter_simple2hash.rb
@@ -1,6 +1,6 @@
 module Fluent
   class Simple2hash < Filter
-    Plugin.register_input('simple2obeject', self)
+    Plugin.register_input('simple2hash', self)
 
     config_param :var, :string, :default => 'value'
 
